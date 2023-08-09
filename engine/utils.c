b88815530654dbbc3812216e46061eea3274ca9e
Konstantinos Fragkoulis <konstantinosfragoulis@proton.me>
2023-08-10 00:07:43 +0300

    Set GameObject Name and Sprite

--- engine/utils.c
+++ engine/utils.c
@@ -1,3 +1,6 @@
+#include <stdlib.h> // malloc
+#include <string.h> // memcpy strcpy
+
 #include "engine.h"
 
 int cstrlen(const char *str) {
@@ -6,4 +9,21 @@ int cstrlen(const char *str) {
         ++length;
     }
     return length;
+}
+
+void SetGameObjectName(GameObject *obj, const char *name) {
+    obj->name = (char *)malloc(cstrlen(name) + 1);
+    strcpy(obj->name, name);
+}
+
+void SetGameObjectSprite(GameObject *obj, int rows, int cols, char (*arr)[cols]) {
+    
+    obj->spriteRenderer->sprite = (char **)malloc(sizeof(char *) * rows);
+    for (int i = 0; i < rows; ++i) {
+        obj->spriteRenderer->sprite[i] = (char *)malloc(cols * sizeof(char));
+        memcpy(obj->spriteRenderer->sprite[i], arr[i], cols);
+    }
+
+    obj->spriteRenderer->rows = rows;
+    obj->spriteRenderer->cols = cols;
 }
