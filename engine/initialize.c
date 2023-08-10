c23d71f6be9e3c2c9d4c2fad9f0f36fbd3d09bc4
Konstantinos Fragkoulis <konstantinosfragoulis@proton.me>
2023-08-10 09:36:56 +0300

    Initialize GameObject array

--- engine/initialize.c
+++ engine/initialize.c
@@ -1,9 +1,19 @@
+#include <stdlib.h> // malloc
+
 #include "engine.h"
+
+GameObject **gameObjectsToDraw[MAX_LAYERS];
 
 void initAll() {
 
     enableRawMode();
     
     initFrameBuffer();
+
+    for(int i = 0; i < MAX_LAYERS; ++i) {
+        gameObjectsToDraw[i] = (GameObject **)malloc(sizeof(GameObject *));
+        gameObjectsToDraw[i][0] = NULL;
+    }
+
 
 }
