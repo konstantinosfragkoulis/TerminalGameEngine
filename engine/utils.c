ced10a425e924d040447d8d4d01145fa8f081733
Konstantinos Fragkoulis <konstantinosfragoulis@proton.me>
2023-08-10 21:25:35 +0300

    Two new functions to create and add a SpriteRenderer to a GameObject

--- engine/utils.c
+++ engine/utils.c
@@ -17,6 +17,7 @@ void SetGameObjectName(GameObject *obj, const char *name) {
     strcpy(obj->name, name);
 }
 
+/* ***Depricated** - Use NewSpriteRenderer() and AddSpriteRendererToGameObject()*/
 void SetGameObjectSprite(GameObject *obj, int rows, int cols, char (*arr)[cols], int layer) {
     
     obj->spriteRenderer->sprite = (char **)malloc(sizeof(char *) * rows);
@@ -30,6 +31,7 @@ void SetGameObjectSprite(GameObject *obj, int rows, int cols, char (*arr)[cols],
 
     obj->spriteRenderer->layer = layer;
 
+    /* Add GameObject with SpriteRenderer to GameObject list to automatically render them each frame */
     if(gameObjectsToDraw[layer][0] == NULL) { // This layer is empty
         gameObjectsToDraw[layer][0] = obj;
         layerElementCount[layer] = 1;
@@ -50,4 +52,35 @@ GameObject* NewGameObject(GameObject *obj, const char* name, const int x, const 
     obj->y = y;
 
     return obj;
+}
+
+SpriteRenderer* NewSpriteRenderer(SpriteRenderer* renderer, const int color, int rows, int cols, char (*arr)[cols], int layer) {
+
+    renderer->sprite = (char **)malloc(sizeof(char *) * rows);
+    for (int i = 0; i < rows; ++i) {
+        renderer->sprite[i] = (char *)malloc(cols * sizeof(char));
+        memcpy(renderer->sprite[i], arr[i], cols);
+    }
+
+    renderer->color = (int) color;
+    renderer->rows = rows;
+    renderer->cols = cols;
+    renderer->layer = layer;
+
+    return renderer;
+}
+
+void AddSpriteRendererToGameObject(GameObject* obj, SpriteRenderer* renderer) {
+    obj->spriteRenderer = renderer;
+
+    /* Add GameObject with SpriteRenderer to GameObject list to automatically render them each frame */
+    if(gameObjectsToDraw[renderer->layer][0] == NULL) { // This layer is empty
+        gameObjectsToDraw[renderer->layer][0] = obj;
+        layerElementCount[renderer->layer] = 1;
+    } else {
+        layerElementCount[renderer->layer]++;
+        const int newSize = (layerElementCount[renderer->layer] + 1) * sizeof(GameObject *);
+        gameObjectsToDraw[renderer->layer] = (GameObject **)realloc(gameObjectsToDraw[renderer->layer], newSize); // Allocate more memory for 1 more GameObject
+        gameObjectsToDraw[renderer->layer][layerElementCount[renderer->layer] - 1] = obj;
+    }
 }
